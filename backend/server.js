2a04c92bed713335aca8a9ee133e8aa4459629f3
sarvjeetkumar012 <sarvjeetkumar829294@gmail.com>
2026-01-29 22:53:33 +0530

    Add

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,15 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+const allowedOrigins = [
+  'http://localhost:3000',
+  process.env.FRONTEND_URL
+].filter(Boolean);
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
